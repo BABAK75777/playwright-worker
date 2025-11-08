2d9b4682bda26379ada4027fc56580c9328715b0
BABAK75777 <136737672+BABAK75777@users.noreply.github.com>
2025-11-07 20:50:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,11 @@ def search(q: str = Query(..., description="Search query")):
 
     try:
         resp = httpx.get(
-    url,
-    params=params,
-    timeout=15.0,
-    follow_redirects=True,
-)
-
+            url,
+            params=params,
+            timeout=15.0,
+            follow_redirects=True,
+        )
         resp.raise_for_status()
     except Exception as e:
         return {
